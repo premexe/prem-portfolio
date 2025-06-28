cdfb96bf173c10172e78e13d0487d9622cce2f74
premkakade2021 <premkakade2021@gmail.com>
2025-06-28 17:01:37 +0000

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-site/', // important for GitHub Pages
   plugins: [react()],
+  base: '/portfolio-site/', // ✅ important for GitHub Pages
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
